a629d00efe72ee2bc6c8cb85d817b976a6341de5
凱恩Kane <33802653+Gary50613@users.noreply.github.com>
2021-05-01 06:02:56 +0800

    Update deploy.sh
    fixing for #1

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ echo "ka1e.co" > CNAME
 git init
 git add .
 git commit -am "update"
-git push -f https://github.com/kane-tw/kane-tw.github.io.git master:main
+git push -f https://github.com/kane-tw/kane-tw.github.io.git HEAD:main
