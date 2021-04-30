d18187b292898e3d25bd0b693d207168501046e2
Gary50613 <50613gary@gmail.com>
2021-04-30 23:12:22 +0800

    making navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,58 @@
 <template>
-  <router-view/>
+    <Navbar/>
+    <transition name="fade">
+        <router-view/>
+    </transition>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Navbar from "@/components/Navbar";
+export default {
+    components: {Navbar}
 }
+</script>
+
+<style>
+
+* {
+    /*outline: #00b7ff solid 3px;*/
+    outline: none;
+    text-decoration: none;
+    margin: 0;
+    padding: 0;
+    color: #c6c6c6;
+    border-radius: 3px;
+}
+
+*:focus {
+    box-shadow: 0 0 0 3px #00b7ff;
+}
+
+body {
+    background-color: #212121;
+}
+
+#app {
+    margin-top: 2rem;
+    font-family: 'Comfortaa', cursive;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .5s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+    opacity: 1;
+}
+
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,10 @@
 <template>
-
+    <div class="nav">
+        <router-link to="/" class="brand">
+            <img class="avatar" src="https://github.com/Gary50613.png?size=128">
+            <span class="title">Kane</span>
+        </router-link>
+    </div>
 </template>
 
 <script>
@@ -9,5 +14,29 @@ export default {
 </script>
 
 <style scoped>
+    .nav {
+        padding: 1rem 7rem;
+        display: flex;
+        text-align: left;
+        align-items: center;
+    }
 
+    .brand {
+        cursor: pointer;
+        display: flex;
+        text-align: left;
+        align-items: center;
+    }
+
+    .avatar {
+        border-radius: 50%;
+        width: 4rem;
+        height: 4rem;
+    }
+
+    .title {
+        margin: 0 2rem;
+        font-weight: bold;
+        font-size: 3rem;
+    }
 </style>
